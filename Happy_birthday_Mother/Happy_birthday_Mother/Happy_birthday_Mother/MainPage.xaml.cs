50f0908be254384870054baa764327e5499af396
shakosmail59 <shakosmail12345@gmail.com>
2020-12-04 14:07:56 -0800

    init

---
+++ Happy_birthday_Mother/Happy_birthday_Mother/Happy_birthday_Mother/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Happy_birthday_Mother
+{
+    public partial class MainPage : ContentPage
+    {
+        public string[] iMAGEmINIONS = new string[] { "hhfghf.jpg",
+                                                    "Cakes.jpg" , "dad.jpg" , " " };
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ImageSlide.ItemsSource = iMAGEmINIONS;
+
+            ImageSlide.PositionChanged += ImageSlide_PositionChanged;
+
+
+        }
+
+        private void ImageSlide_PositionChanged(object sender, PositionChangedEventArgs e)
+        {
+            MinionsGIF.IsVisible = e.CurrentPosition == iMAGEmINIONS.Length - 1;
+        }
+    }
+}
